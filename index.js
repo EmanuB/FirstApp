713b08e96b08176badde777124927deb8c1b53ea
EmanuB <emanuelbandeira@alu.ufc.br>
2021-08-09 14:28:22 -0300

    first_app 0.3.0
    Finalização dos botões e páginas de "home", "cadastro" e "about" da aplicação e melhoras na organização do código

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
-const app= expresss();
+const app= express();
+const routes = require("./routes");
+//era do routes, foi para o index por causa de um erro na aplicação
+const expressLayouts = require("express-ejs-layouts");
 
-const port=3000;
+const port=3030;
 const address="localhost";
 
+ //ativa uso do ejs e do Express-ejs-layouts //Era do routes, foi para o index por causa de um erro na aplicação
+ app.set('view engine', 'ejs');
+ app.use(expressLayouts);
+
+//aula--Criando usando rotas simples que estão no arquivo routes.
+app.use('/',routes);
 //Criando um servidor simples com Node.js
 
 const server = app.listen(port,address, ()=>{
     let host = server.address().address;
     let port = server.address().port;
-    console.log(`Servidor executando no endereço ${host} e potta ${port}`);
+    console.log(`Servidor executando no endereço ${host} e porta ${port}`);
 });
--- routes.js
+++ routes.js
@@ -2,16 +2,31 @@
 
 const express = require('express');
 const router = express.Router();
+const app = express();
 
-//Home page route
-router.get('/', function(req, res){
-    res.send('Wiki home page');
+
+router.use(express.static('public'));
+
+
+router.get('/',(req,res)=>{
+    res.render('pages/home');
 });
+router.get('/about', (req,res)=>{
+    res.render('pages/about');
+});
+router.get('/cadastro', (req,res)=>{
+    res.render('pages/cadastro');
+});
+//Home page route
+// router.get('/', function(req, res){
+//     res.send('Wiki home page');
+// });
 
 //About page route (pq isso tá em inglês??)
-router.get('/about', function(req, res){
-    res.send('About this wiki');
-});
+// router.get('/about', function(req, res){
+//     res.send('About this wiki');
+// });
 
+//exemplo de rota: http://localhost:3030/index.html
 module.exports = router
 //parei no slide 25, no slide 28 tem mais códigos mas eles estão desconexos???
